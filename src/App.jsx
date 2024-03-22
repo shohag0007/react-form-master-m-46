eac2c5dffcd40b19998f2993e49fe84003d77d45
shohag0007 <merinedrive007@gmail.com>
2024-03-22 17:42:50 +0600

    reusable form

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,13 @@ import ReuseableForm from './components/ReuseableForm/ReuseableForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
- 
+  const handleSignUpSubmit = data =>{
+    console.log('sign up data', data);
+ }
 
+ const handleUpdateProfile = data =>{
+  console.log('update profile', data);
+ }
   return (
     <>
       
@@ -17,8 +22,18 @@ function App() {
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
       {/* <HookForm></HookForm> */}
-      <ReuseableForm formTitle={'Sign Up'}></ReuseableForm>
-      <ReuseableForm formTitle={'Profile Update'} submitBtnText='Update'></ReuseableForm>
+      <ReuseableForm formTitle={'Sign Up'} handleSubmit={handleSignUpSubmit}>
+        <div>
+          <h2>Sign Up</h2>
+          <p>Please Sign Up Right Now</p>
+        </div>
+      </ReuseableForm>
+      <ReuseableForm formTitle={'Profile Update'} handleSubmit={handleUpdateProfile} submitBtnText='Update'>
+        <div>
+          <h2>Update Profile</h2>
+          <p>Always keep your profile Updated</p>
+        </div>
+      </ReuseableForm>
       
     </>
   )
--- src/components/ReuseableForm/ReuseableForm.jsx
+++ src/components/ReuseableForm/ReuseableForm.jsx
@@ -1,22 +1,30 @@
 
 
-const ReuseableForm = ({formTitle, submitBtnText='Submit'}) => {
- const handleSubmit = e =>{
+const ReuseableForm = ({formTitle, handleSubmit, submitBtnText='Submit', children}) => {
+ const handleLocalSubmit = e =>{
     e.preventDefault();
+    const data = {
+        name: e.target.name.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+
+
+    }
+    handleSubmit(data);
  }
 
 
     return (
         <div>
-            <h2>{formTitle}</h2>
-             <form onSubmit={handleSubmit}>
+            {children}
+             <form onSubmit={handleLocalSubmit}>
                 <input type="text" name="name"/>
                 <br/>
                 <input type="email" name="email" id=""/>
                 <br/>
                 <input type="password" name="password"/>
                 <br/>
-                <input type="submit" value={submitBtnText} submitBtnText='Update' />
+                <input type="submit" value={submitBtnText}  />
             </form>
         </div>
     );
